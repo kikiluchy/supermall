0f164b8009a138fff79462bf5fea355d96847af1
itheima <itheima@itcast.cn>
2021-03-05 21:03:29 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
   <div id="app">
-    <router-view/>
-    <main-tab-bar/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-  import MainTabBar from 'components/content/mainTabbar/MainTabBar'
+import HelloWorld from './components/HelloWorld.vue'
 
-  export default {
-    name: 'app',
-    components: {
-      MainTabBar
-    }
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
   }
+}
 </script>
 
 <style>
-  @import "assets/css/base.css";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
